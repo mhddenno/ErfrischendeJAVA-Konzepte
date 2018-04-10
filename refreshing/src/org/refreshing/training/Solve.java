dbbbfbb9b5161b13d9bd7d9ce321b6cce07cced3
Mohamad Denno <dennokhmhd@gmail.com>
2018-04-10 19:09:07 +0200

    Initiate the info reader

--- refreshing/src/org/refreshing/training/Solve.java
+++ refreshing/src/org/refreshing/training/Solve.java
@@ -2,6 +2,7 @@ package org.refreshing.training;
 import java.util.*;
 
 public class Solve {
+	private static final Scanner scanner=new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
