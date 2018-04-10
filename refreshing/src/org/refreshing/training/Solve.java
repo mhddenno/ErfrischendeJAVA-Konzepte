72102a4a4e0a34c54f02db07efbdc3537a92fbc4
Mohamad Denno <dennokhmhd@gmail.com>
2018-04-10 19:10:01 +0200

    Initializing the main

--- refreshing/src/org/refreshing/training/Solve.java
+++ refreshing/src/org/refreshing/training/Solve.java
@@ -6,7 +6,14 @@ public class Solve {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int scores[]=new int[] {100,100,50,40,40,20,10};
+		//5 25 50 120
+		int alice[]=new int[] {5,25,50,120};
+		
+		int result[]=Solve.climbingLeaderboard(scores, alice);
+		for(int i : result) {
+			System.out.println(i);
+		}
 		
 		
 	}
