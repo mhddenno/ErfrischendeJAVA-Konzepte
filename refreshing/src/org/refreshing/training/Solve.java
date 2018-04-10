6af54c3141c5a35aa70492df5c027c0b79efc0e2
Mohamad Denno <dennokhmhd@gmail.com>
2018-04-10 19:06:10 +0200

    First commit

---
+++ refreshing/src/org/refreshing/training/Solve.java
@@ -0,0 +1,13 @@
+package org.refreshing.training;
+import java.util.*;
+
+public class Solve {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+	
+}
