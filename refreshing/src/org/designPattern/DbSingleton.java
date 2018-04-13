3c5b4b8928380b9331cd3f3c85b99b0d96c1e629
Mohamad Denno <dennokhmhd@gmail.com>
2018-04-13 08:05:04 +0200

    Adding Singleton creational design pattern

--- refreshing/src/org/designPattern/DbSingleton.java
+++ refreshing/src/org/designPattern/DbSingleton.java
@@ -1,5 +1,13 @@
 package org.designPattern;
 
 public class DbSingleton {
-
+	private static DbSingleton instance= new DbSingleton();
+	
+	private DbSingleton() {
+		
+	}
+	
+	public static DbSingleton getInstance() {
+		return instance;
+	}
 }
