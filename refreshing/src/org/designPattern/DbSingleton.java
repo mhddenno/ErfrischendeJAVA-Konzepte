6e55148e06058c0823e9f035fef1f75aa04e338e
Mohamad Denno <dennokhmhd@gmail.com>
2018-04-13 09:06:44 +0200

    upgrade the Singleton to be Lazy loaded

--- refreshing/src/org/designPattern/DbSingleton.java
+++ refreshing/src/org/designPattern/DbSingleton.java
@@ -1,13 +1,17 @@
 package org.designPattern;
 
 public class DbSingleton {
-	private static DbSingleton instance= new DbSingleton();
-	
+	//private static DbSingleton instance= new DbSingleton();
+	//Convert it to Lazy loaded
+	private static DbSingleton instance=null;
 	private DbSingleton() {
 		
 	}
 	
 	public static DbSingleton getInstance() {
+		if(instance==null) {
+			instance= new DbSingleton();
+		}
 		return instance;
 	}
 }
